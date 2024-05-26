b07c7fc86779d0346434d71f3b4847f35259e310
Pravin <pravin.jeyaramn@student.curtin.edu.au>
2024-05-27 00:40:48 +0800

    added base functions for life path calculation, added main

---
+++ assignment_ISAD.java
@@ -0,0 +1,22 @@
+public class NumberCalculator {
+    // Method to calculate the Life Path Number from the birth year
+    public static int lifePath(int birthYear) {
+       int lifePath = 0;
+       return lifePathNum;
+    }
+
+    // Method to determine the lucky color based on the Life Path Number
+    public static String colour(int lifePathNum) {
+        String colour = "";
+        return colour;
+    }
+
+    // Method to check if a number is a master number (11, 22, 33)
+    public static boolean isMasterNumber(int lifePathNum) {
+        return true;
+    }
+
+    // Main method to test the functionality
+    public static void main(String[] args) {
+    }
+}
